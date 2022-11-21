522a58b9492c2c2ab618724b0eda10c95d79e80d
lilianafss <liliana.sntss@eduge.ch>
2022-11-21 09:17:18 +0100

    map test

---
+++ 9b9962b5caef778a16cd6d07ad798146.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="9b9962b5caef778a16cd6d07ad798146" tilewidth="32" tileheight="32" tilecount="1276" columns="22">
+ <image source="../../../Desktop/9b9962b5caef778a16cd6d07ad798146.jpg" width="710" height="1879"/>
+</tileset>
